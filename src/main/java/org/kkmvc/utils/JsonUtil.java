bc3774fd16e9280e8e2080041f5cecda658912c4
KKys <kkys.china@gmail.com>
2017-11-09 12:43:39 +0800

    utils commit

--- src/main/java/org/kkmvc/utils/JsonUtil.java
+++ src/main/java/org/kkmvc/utils/JsonUtil.java
@@ -1,14 +1,47 @@
 package org.kkmvc.utils;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * json工具类
  */
 public final class JsonUtil {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CodecUtil.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonUtil.class);
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    /**
+     * 将POJO转为json
+     */
+    public static <T> String toJson(T obj) {
+        String json;
+        try {
+            json = OBJECT_MAPPER.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            LOGGER.error("convert POJO to JSON failure", e);
+            throw new RuntimeException(e);
+        }
+        return json;
+    }
+
+    /**
+     * 将JSON转为POJO
+     */
+    public static <T> T fromJson(String json, Class<T> type) {
+        T pojo;
+        try {
+            pojo = OBJECT_MAPPER.readValue(json, type);
+        } catch (IOException e) {
+            LOGGER.error("convert JSON to POJO failure", e);
+            throw new RuntimeException(e);
+        }
+        return pojo;
+    }
 }
