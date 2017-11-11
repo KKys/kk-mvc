09a7a5096b3d16e3f4b8e9f51e30e996b5a06f13
KKys <kkys.china@gmail.com>
2017-11-11 09:31:11 +0800

    aop commit

---
+++ src/main/java/org/kkmvc/annotation/Aspect.java
@@ -0,0 +1,16 @@
+package org.kkmvc.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 切面注解
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Aspect {
+
+    /**
+     * 注解
+     */
+    Class<? extends Annotation> value();
+}
